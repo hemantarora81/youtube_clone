bc12c44ce8b9205470914140d847bfaf7809df4b
Hemant Arora <arorahemant75@gmail.com>
2023-07-01 22:40:16 +0530

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   <Stack direction="row" alignItems="center" p={2} sx={{ position: 'sticky', background: '#e6dfdf', top: 0, justifyContent: 'space-between',borderBottom:'1px solid #aba7a6',marginBottom:'5px' }}>
     <Link to='/' style={{ display: 'flex', alignItems: 'center' }}>
         <img src={logo} alt='logo' height={45} />
-        <span style={{fontWeight:'700',marginLeft:'2px' ,fontSize:'32px',color:'#575251',fontStyle:'normal'}}>Youtube</span>
+        <span className='youtube-write' style={{fontWeight:'700',marginLeft:'2px' ,fontSize:'32px',color:'#575251',fontStyle:'normal'}}>Youtube</span>
     </Link>
     <SearchBar  />
     
