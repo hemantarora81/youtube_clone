8a7f5881b7af5a0d741033ba04d909dd7eb46d88
Hemant Arora <arorahemant75@gmail.com>
2023-08-24 16:40:09 +0530

    Responsiveness

--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -3,8 +3,6 @@
 // import { Sidebar, Videos } from './';
 // import { fetchFromAPI } from '../utils/fetchFromAPI';
 
-
-
 // const Feed = () => {
 //   const [selectedCategory, setSelectedCategory] = useState('New');
 //   const [videos, setVideos] = useState(null);
@@ -35,29 +33,31 @@
 
 // export default Feed
 import React, { useEffect, useState } from "react";
-import { Box,  Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos} from "./";
+import { Videos } from "./";
 
 const Feed = () => {
   const [videos, setVideos] = useState(null);
-  const {searchTerm } = useParams();
+  const { searchTerm } = useParams();
   useEffect(() => {
     setVideos(null);
 
-    fetchFromAPI(`search?part=snippet&q=${searchTerm}}`)
-      .then((data) => setVideos(data.items))
-    }, []);
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
 
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-    <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "black" }}>
-     Search Results for:  <span style={{ color: "#FC1503" }}>{searchTerm} </span>videos
-    </Typography>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm} </span>videos
+      </Typography>
 
-    <Videos videos={videos} />
-  </Box>
+      <Videos videos={videos} />
+    </Box>
   );
 };
 
